337219a816de1ff286d2c54588dff64ca1ecb7cc
Mko Galoyan <galoyanmko@gmail.com>
2018-04-25 10:21:49 +1000

    added grid display

--- src/GridLock.java
+++ src/GridLock.java
@@ -1,19 +1,47 @@
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
  
+
+
+
 public class GridLock extends Application {
+	private ArrayList<Square> grid = new ArrayList<Square>();
+
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("GridLock");
+    	
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 600, Color.WHITE);
         
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 600, 600));
+        
+        //populating grid array with squares
+        for(int i = 0; i < 6; i++) {
+        	for(int j = 0; j < 6; j++) {
+        		Square square = new Square(j*100,i*100 , 100);
+                square.setFill(Color.TRANSPARENT);
+                square.setStroke(Color.BLACK);
+                grid.add(square);
+        	}
+        	
+        }
+        
+        
+      
+         //adding arraylist of grid to the window
+        root.getChildren().addAll(grid);
+ 
+        primaryStage.setTitle("GridLock");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,22 @@
+import javafx.scene.shape.Rectangle;
 
-public class Square {
-
+public class Square extends Rectangle{
+	private double squareSize;
+	private boolean occupied;
+	
+	public Square(double x, double y, double size) {
+		super(x, y,size, size);
+		occupied = false; 
+		squareSize = size;
+	}
+	
+	public double getSquareSize(){
+		return squareSize;
+	}
+	
+	public boolean isOccupied() {
+		return occupied;
+	}
+	
+	
 }
